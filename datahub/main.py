d0514ec9b3e0559e2ec299c5b1496a1f08053e88
Nika-Keks <as79020947761@gmail.com>
2022-10-13 15:33:47 +0300

    added datahub configure args

--- datahub/main.py
+++ datahub/main.py
@@ -2,19 +2,21 @@ import argparse
 
 from datahub import utils
 
-comands = {
+commands = {
     "configure": utils.cli_configure
 }
 
 def arg_parser():
     parser = argparse.ArgumentParser(description="""
-    Datahub utils to manage the data repository
+    Datahub utils to manage the data repository configuration
     """)
-    parser.add_argument("comand", type=str, choices=comands.keys())
+    parser.add_argument("command", type=str, choices=commands.keys(), 
+        help="utils command")
 
     return parser
 
 def main(argv: str):
     parser = arg_parser()
-    args = parser.parse_args(argv[1:])
-    comands[args.comand]()
+    args = parser.parse_args(argv[1:2])
+    
+    commands[args.command](argv[2:])
